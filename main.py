04d2bf92c3f6789926fb57c08862f19baf244a16
Joanna Robakowska <86994@student.ue.poznan.pl>
2022-05-29 10:25:21 +0200

    kolejne podejscie

--- main.py
+++ main.py
@@ -1,69 +1,85 @@
-class ksiazka:
-    def __init__(self, tytul, autor, rok):
-        self.tytul = tytul
-        self.autor = autor
-        self.rok = rok
-        self.quantity = 1
+class Biblioteka(object):
+    def __init__(self):
+        self.ksiazki = []
+        self.egzemplarze=[]
+        self.czytelnicy= {}
+        self.tytuly=[]
+        self.komunikaty=[]
 
+    def addKsiazka(self, ksiazka):
+        self.ksiazki.append(ksiazka)
 
-class osoba:
-    def __init__(self, imie):
-        self.imie = imie
-        self.pozyczonych = 0
-        self.ksiazki = {}
-
-
-n = input()
-ksiazki = {}
-ludzie = {}
-
-
-def commands(command, tytul, autor, rok=None):
-    def dodaj(tytul, autor, rok):
-        if tytul in ksiazki:
-            ksiazki[tytul].quantity += 1
+    def addEgzemplarz(self,egzemplarz):
+        egzemplarz1 = list(egzemplarz)
+        if egzemplarz1[1:3] in self.ksiazki:
+            self.egzemplarze.append(egzemplarz1[1:3])
+            self.tytuly.append(egzemplarz1[1])
+            self.komunikaty.append('True')
         else:
-            ksiazki[tytul] = ksiazka(tytul, autor, rok)
-        return True
+            library.addKsiazka(egzemplarz1[1:3])
+            self.egzemplarze.append(egzemplarz1[1:3])
+            self.tytuly.append(egzemplarz1[1])
+            self.komunikaty.append('True')
 
-    def wypozycz(imie, tytul):
-        if ludzie[imie].pozyczonych == 3:
-            return False
+    def liczenie(self):
+        listafinal=[]
+        listasort=[]
+        for ksiazka in self.ksiazki:
+            a = self.egzemplarze.count(ksiazka)
+            ksiazka1 = [ksiazka[0], ksiazka[1], a]
+            listafinal.append(ksiazka1)
+        listasort = sorted(listafinal, key=lambda x: x[0])
+        for element in listasort:
+            element1 = (element[0], element[1], element[2])
+            print(element1)
+
+    def czytelnik(self, nazwisko, tytul):
+        if nazwisko in self.czytelnicy:
+            if len(self.czytelnicy[nazwisko]) < 3 and tytul not in self.czytelnicy[nazwisko]:
+                self.czytelnicy[nazwisko].append(tytul)
+                self.tytuly.remove(tytul)
+                self.komunikaty.append('True')
+            else:
+                self.komunikaty.append('False')
         else:
-            if tytul in ksiazki:
-                if ksiazki[tytul].quantity >= 1 and tytul not in ludzie[imie].ksiazki:
-                    ludzie[imie].pozyczonych += 1
-                    ksiazki[tytul].quantity -= 1
-                    ludzie[imie].ksiazki[tytul] = tytul
-                    return True
-            return False
+            self.czytelnicy[nazwisko]=[]
+            self.czytelnicy[nazwisko].append(tytul)
+            self.tytuly.remove(tytul)
+            self.komunikaty.append('True')
 
-    def donate(imie, tytul):
-        if imie in ludzie:
-            if tytul in ludzie[imie].ksiazki:
-                ludzie[imie.pozyczonych] -= 1
-                ksiazki[tytuly].quantity += 1
-                del ludzie[imie].ksiazki[tytul]
-                return True
-        return False
+    def wypozycz(self,nazwisko,tytul):
+        if tytul in self.tytuly:
+            self.czytelnik(nazwisko,tytul)
+        else:
+            self.komunikaty.append('False')
 
-    if command == 'dodaj':
-        return dodaj(tytul, autor, rok)
-    elif command == 'wypozycz':
-        if tytul not in ludzie:
-            ludzie[tytul] = osoba(tytul)
-        return wypozycz(imie=tytul, tytul=autor)
-    elif command == 'oddaj':
-        return donate(imie=tytul, tytul=autor)
+    def oddaj(self,nazwisko,tytul):
+        if nazwisko in self.czytelnicy:
+            if tytul in self.czytelnicy[nazwisko]:
+                self.czytelnicy[nazwisko].remove(tytul)
+                self.tytuly.append(tytul)
+                self.komunikaty.append('True')
+            else:
+                self.komunikaty.append('False')
 
+        else:
+            self.komunikaty.append('False')
 
-for i in range(int(n)):
-    temp = eval(input())
-    command = temp[0].strip()
-    tytul = temp[1].strip()
-    autor = temp[2].strip()
-    if len(temp) > 3:
-        rok = temp[3]
-        print(commands(command, tytul, autor, rok))
-    else:
-        print(commands(command, tytul, autor))
+    def drukuj(self):
+        for komunikat in self.komunikaty:
+            print(komunikat)
+
+library = Biblioteka()
+
+k = int(input())
+
+for i in range(0, k):
+    wejscie = eval(input())
+    if wejscie[0] == "dodaj":
+        library.addEgzemplarz(wejscie)
+    elif wejscie[0] == "wypozycz":
+        library.wypozycz(wejscie[1],wejscie[2])
+    elif wejscie[0] == "oddaj":
+        library.oddaj(wejscie[1],wejscie[2])
+
+library.drukuj()
